a8291167318551754df6d0bb4711a50c1b607ee6
Mikegg89 <Mikegg21@gmail.com>
2018-05-08 01:19:40 -0500

    most of the site fleshed out

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var connection = require('./lib/config/config.js');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,8 +13,33 @@ app.get("/", function(req, res) {
   res.render('home', { cat: cat });
 });
 
-app.post("/register", function(req, res) {
-  console.log(req, res);
+app.get("/subscribed", function(req, res) {
+  res.render('subscribed');
+});
+
+app.post("/", function(req, res) {
+  var subscriber = {
+    email: req.body.email
+  }
+  connection.connection.query('INSERT INTO subscribers SET ?', subscriber, function(error, results) {
+    if (error) console.log(error.message);
+    console.log(results);
+  });
+  res.render('home');
+});
+
+app.post("/quote", function(req, res) {
+  var customer = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    quote: req.body.quote
+  }
+  connection.connection.query('INSERT INTO customers SET ?', customer, function(error, results) {
+    if (error) console.log(error.message);
+    console.log(results);
+  });
+  res.render('quote');
 });
 
 app.listen(8080, function() {
---
+++ lib/assets/schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE cake_shop;
+
+USE cake_shop;
+
+CREATE TABLE subscribers (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(100) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()  
+);
+
+CREATE TABLE customers (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  quote VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
