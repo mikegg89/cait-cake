580f2e9473ee16936fdc95a6e6b06dbc7219f799
Micheal Giles <mikegg21@gmail.com>
2018-11-22 08:04:45 -0600

    fixed page scroll

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,7 +1,7 @@
 $('.option').children('a').click(function (e) {
     e.preventDefault();
     var postId = $($(this).attr('href'));
-    var postLocation = postId.offset().top - 60;
+    var postLocation = postId.offset().top;
 
     $('html, body').animate({scrollTop:postLocation},'slow');
 });
